b6bbae9adc8b3fe26ddd5629264e005a378f46b5
senthilvelk <32328757+senthilvelk@users.noreply.github.com>
2021-02-04 13:43:01 +0530

    Add files via upload

---
+++ ServiceAvailabilityTest/Main.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+using System.IO;
+
+namespace ServiceAvailabilityTest
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtTimer = new System.Windows.Forms.TextBox();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.lblFile = new System.Windows.Forms.Label();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.timerInterval = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // txtTimer
+            // 
+            this.txtTimer.Location = new System.Drawing.Point(171, 84);
+            this.txtTimer.Name = "txtTimer";
+            this.txtTimer.Size = new System.Drawing.Size(100, 20);
+            this.txtTimer.TabIndex = 0;
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.Location = new System.Drawing.Point(42, 87);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(99, 13);
+            this.lblTimer.TabIndex = 1;
+            this.lblTimer.Text = "Interval (In minutes)";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(171, 134);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(100, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start TiP test";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(171, 188);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(100, 23);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Stop TiP test";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // lblFile
+            // 
+            this.lblFile.AutoSize = true;
+            this.lblFile.Location = new System.Drawing.Point(63, 33);
+            this.lblFile.Name = "lblFile";
+            this.lblFile.Size = new System.Drawing.Size(78, 13);
+            this.lblFile.TabIndex = 4;
+            this.lblFile.Text = "Select URL file";
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            this.openFileDialog.FilterIndex = 2;
+            this.openFileDialog.InitialDirectory = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            this.openFileDialog.RestoreDirectory = true;
+            this.openFileDialog.Title = "Browse Text Files";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(171, 33);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(100, 23);
+            this.btnBrowse.TabIndex = 5;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(348, 33);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(111, 13);
+            this.lblStatus.TabIndex = 6;
+            this.lblStatus.Text = "Status messages here";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(620, 289);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.lblFile);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblTimer);
+            this.Controls.Add(this.txtTimer);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Main";
+            this.Text = "TiP Test";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTimer;
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label lblFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Timer timerInterval;
+    }
+}
+
---
+++ ServiceAvailabilityTest/Main.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.Net.Mail;
+using System.Configuration;
+using System.Text.RegularExpressions;
+
+namespace ServiceAvailabilityTest
+{
+    public partial class Main : Form
+    {
+        public int counter;
+        public Main()
+        {
+            InitializeComponent();
+            counter = 0;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if(txtTimer.Text != "" && openFileDialog.FileName != "")
+            {
+                if (!Regex.Match(txtTimer.Text, "^[0-9]*$").Success)
+                {
+                    // first name was incorrect  
+                    MessageBox.Show("Invalid Time Interval", "Message", MessageBoxButtons.OK,  MessageBoxIcon.Error);
+                    txtTimer.Focus();
+                    return;
+                }
+                if (!timerInterval.Enabled)
+                {
+                    // First run
+                    lblStatus.Text = "Starting TiP test";
+                    lblStatus.Refresh();
+                    Thread.Sleep(1000);
+                    SendPing(sender,e);
+
+                    // Get duration
+                    int duration = Convert.ToInt32(txtTimer.Text) * 1000 * 60; //Convert timer in minutes to seconds
+
+                    timerInterval.Interval = duration;
+                    timerInterval.Start();
+                    timerInterval.Tick += new EventHandler(SendPing);
+                }
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if(timerInterval.Enabled)
+            {
+                timerInterval.Stop();
+                lblStatus.Text = "Status messages here";
+                lblStatus.Refresh();
+            }
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            openFileDialog.ShowDialog();
+        }
+
+        public void SendPing(object sender, EventArgs e)
+        {
+            try
+            {
+                counter++;
+                // Get URLs
+                string textFile = openFileDialog.FileName;
+                string[] lineItems = File.ReadAllLines(textFile);
+
+                Ping pingSender = new Ping();
+                PingOptions options = new PingOptions();
+
+                // Use the default Ttl value which is 128,
+                // but change the fragmentation behavior.
+                options.DontFragment = true;
+
+                // Create a buffer of 32 bytes of data to be transmitted.
+                string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+                byte[] buffer = Encoding.ASCII.GetBytes(data);
+                int timeout = 120;
+
+                foreach (string url in lineItems)
+                {
+                    // Ping the address
+                    lblStatus.Text = string.Format("TiP test for {0}", url);
+                    lblStatus.Refresh();
+                    Thread.Sleep(1000);
+
+                    PingReply reply = pingSender.Send(url, timeout, buffer, options);
+                    if (reply.Status == IPStatus.Success)
+                    {
+                        lblStatus.Text += string.Format("\n\nAddress: {0}", reply.Address.ToString());
+                        lblStatus.Text += string.Format("\nRoundTrip time: {0}", reply.RoundtripTime);
+                        lblStatus.Text += string.Format("\nTime to live: {0}", reply.Options.Ttl);
+                        lblStatus.Text += string.Format("\nPing status: Success");
+                        lblStatus.Refresh();
+                        Thread.Sleep(1000);
+                    }
+                    else
+                    {
+                        // Send mail to group in case of ping failure
+                        string mailBody = string.Format("TiP test failure for URL: {0}", url);
+                        SendEmail("v-skrishnasa@microsoft.com", null, "TiP Test failure from factory", mailBody, null);
+                    }
+                }
+                lblStatus.Text = "Waiting for next schedule";
+                lblStatus.Refresh();
+                Console.WriteLine("Counter {0}", counter);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Error: {0}", ex.Message);
+            }
+        }
+
+        private void SendEmail(string recipient, string cc, string subject, string body, Attachment attachment)
+        {
+            MailMessage msg = new MailMessage(ConfigurationManager.AppSettings["SendFrom"], recipient, subject, body);
+
+            msg.IsBodyHtml = true;
+
+            if (cc != null && cc.Length != 0)
+            {
+                msg.CC.Add(cc);
+            }
+
+            if (attachment != null)
+            {
+                msg.Attachments.Add(attachment);
+            }
+
+            SmtpClient smtpclient = new SmtpClient(ConfigurationManager.AppSettings["SmtpServer"]);
+            smtpclient.UseDefaultCredentials = true;
+            smtpclient.Send(msg);
+        }
+    }
+}
